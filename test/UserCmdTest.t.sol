02434c25aeb211b216bef37e3f417e8827003b45
Leandro Faria <lee.lara1219@gmail.com>
2024-06-21 13:27:52 -0500

    hotpath issue

--- test/UserCmdTest.t.sol
+++ test/UserCmdTest.t.sol
@@ -7,16 +7,20 @@ import "../src/interface/IDexContract.sol";
 
 contract UserCmdTest is Test {
     IDexContract dex;
+    IDexContract hotPath;
     address eth = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE; // Dummy ETH address
     address dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F; // DAI address on mainnet
     uint256 amount = 36000;
     uint amount2 = 3333;
     uint128 sqrtPrice = toSqrtPrice(amount2);
     uint8 initCode = 71;
+    uint8 constant BASE_SIDE_SETTLE = 0x1; // 00000001
+    uint8 constant QUOTE_SIDE_SETTLE = 0x2; // 00000010
 
     function setUp() public {
         vm.createSelectFork("https://artio.rpc.berachain.com"); // Fork Berachain at the latest block
         dex = IDexContract(0x039fb2fce7Afa77a1f7B045162B34b9037BD2B53); // Use the deployed contract address
+        hotPath = IDexContract(0x8DE058ec8F64B60431EB9AAee95C7266d0d5C311);
     }
 
     function toSqrtPrice(uint256 price) internal pure returns (uint128) {
@@ -38,7 +42,12 @@ contract UserCmdTest is Test {
         (bool success, bytes memory data) = address(dex).call{value: 10**15, gas: 6000000}(abi.encodeWithSignature("userCmd(bytes)", initPoolCmd));
         require(success, "Transaction failed");
 
-
+        bytes memory addToPoolCmd = abi.encode(2, 3, eth, dai, 0, 0, 0, 1, 0, 0, BASE_SIDE_SETTLE, address(0));
+        (bool success1, bytes memory data1) = address(hotPath).call{value: 10**15, gas: 6000000}(abi.encodeWithSignature("userCmd(bytes)", addToPoolCmd));
+        require(success1, "Transaction Failed");
+        console.log("here maybe?");
+        console.logBytes(data1);
+        (int128 baseFlow, int128 quoteFlow) = abi.decode(data1, (int128 , int128 ));
         // Add assertions to verify the expected state changes
         // assertEq(dex.someStateVariable(), expectedValue);
     }
