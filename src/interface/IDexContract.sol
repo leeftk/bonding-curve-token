c980b1f6c539d6612af6798dc4535b68b6533fae
0xnirlin <ahmadhameedkhan80@gmail.com>
2024-06-22 16:36:05 +0500

    brain fried

--- src/interface/IDexContract.sol
+++ src/interface/IDexContract.sol
@@ -2,17 +2,32 @@
 pragma solidity ^0.8.0;
 
 interface IDexContract {
-    function acceptCrocProxyRole(address, uint16) external returns (bool);
-    function protocolCmd(bytes calldata cmd) external;
-    function userCmd(bytes calldata cmd) external payable;
-    function wbera() external view returns (address);
-
     event CrocKnockoutCross(
-        bytes32 indexed pool,
-        int24 indexed tick,
-        bool isBid,
-        uint32 pivotTime,
-        uint64 feeMileage,
-        uint160 commitEntropy
+        bytes32 indexed pool, int24 indexed tick, bool isBid, uint32 pivotTime, uint64 feeMileage, uint160 commitEntropy
     );
+
+    function acceptCrocDex() external pure returns (bool);
+    function protocolCmd(uint16 callpath, bytes memory cmd, bool sudo) external payable;
+    function readSlot(uint256 slot) external view returns (uint256 data);
+    function swap(
+        address base,
+        address quote,
+        uint256 poolIdx,
+        bool isBuy,
+        bool inBaseQty,
+        uint128 qty,
+        uint16 tip,
+        uint128 limitPrice,
+        uint128 minOut,
+        uint8 reserveFlags
+    ) external payable returns (int128 baseQuote, int128 quoteFlow);
+    function userCmd(uint16 callpath, bytes memory cmd) external payable returns (bytes memory);
+    function userCmdRelayer(
+        uint16 callpath,
+        bytes memory cmd,
+        bytes memory conds,
+        bytes memory relayerTip,
+        bytes memory signature
+    ) external payable returns (bytes memory output);
+    function userCmdRouter(uint16 callpath, bytes memory cmd, address client) external payable returns (bytes memory);
 }
